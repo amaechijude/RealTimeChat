2d6a4cc6aeb498fa4cbd7a9d34205b3f1de2d40b
amaechijude <ugwuamaechi5@gmail.com>
2024-06-09 18:09:56 +0100

    second

--- chatapp/views.py
+++ chatapp/views.py
@@ -23,13 +23,16 @@ def index(request):
 def chatroom(request, room_name, username):
     if request.method == 'POST':
         chat = request.POST['chat']
-        new_chat = Message(room=room_name, author=username, content=chat)
+        room_name = request.POST['room_name']
+        room = Room.objects.get(room_name=room_name)
+        new_chat = Message.objects.create(room=room, author=username, content=chat)
         new_chat.save()
+        #return render('chatroom')
     room = Room.objects.get(room_name=room_name)
     chats = Message.objects.filter(room=room)
     
     context = {
-        "chats": chat,
+        "chats": chats,
         "username": username,
         "room": room,
     }
