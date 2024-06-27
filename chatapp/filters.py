f70259530b646a796db15abb6535840eed212ec9
amaechijude <ugwuamaechi5@gmail.com>
2024-06-27 08:40:16 +0100

    hello

--- chatapp/filters.py
+++ chatapp/filters.py
@@ -2,11 +2,10 @@ import django_filters
 from .models import Room
 
 class RoomFilter(django_filters.FilterSet):
-    room_name = django_filters.CharFilter(lookup_expr='qs')
+    room_name = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Room
         fields = ['room_name']
-
 
 
