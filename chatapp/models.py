628e4a61c49d1ca7d6ca99dccf0bd5d7f6935635
amaechijude <ugwuamaechi5@gmail.com>
2024-06-27 17:09:41 +0100

    hello

--- chatapp/models.py
+++ chatapp/models.py
@@ -32,7 +32,7 @@ class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     content = models.TextField()
-    image = ResizedImageField(quality=70, upload_to="media/chats/images", null=True)
+    image = ResizedImageField(blank=True, quality=70, upload_to="media/chats/images", null=True)
     file = models.FileField(blank=True, null=True, upload_to='media/chats/files')
     created_at = models.DateTimeField(auto_now_add=True)
     
