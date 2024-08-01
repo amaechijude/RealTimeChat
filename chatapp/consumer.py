55a96529dfe124bd6150edfdd9e342916b955fad
amaechijude <ugwuamaechi5@gmail.com>
2024-08-01 09:44:10 +0100

    fixed streaming to all room members

--- chatapp/consumer.py
+++ chatapp/consumer.py
@@ -1,47 +1,10 @@
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-from django.shortcuts import get_object_or_404
-from .models import Room, RoomChat
 import json
-from django.template.loader import render_to_string
-from asgiref.sync import async_to_sync, sync_to_async
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import Room, RoomChat
 from channels.db import database_sync_to_async
+from channels.layers import get_channel_layer
 
-from logging import getLogger
-
-logger = getLogger(__name__)
-
-class ChatRoomConsumer(WebsocketConsumer):
-    #connect method
-    def connect(self):
-        #user object
-        self.user = self.scope['user']
-        #room name from the url
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        #chat room instance
-        self.chatroom = get_object_or_404(Room, room_name=self.room_name)
-
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_name, self.channel_name
-        )
-        self.accept()
-
-
-    #recieving messages. receives the messages as textdata
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        content = text_data_json['content']
-        author = self.user.profile
-        room = self.chatroom
-
-        new_chat = RoomChat.objects.create(room=room,author=author,content=content)
-
-        new_chat_json = {
-            "content": new_chat.content,
-            "author": new_chat.author.user.username,   
-        }
-
-        self.send(text_data=json.dumps(new_chat_json))
-        
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -68,27 +31,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.content = text_data_json['content']
         self.username = text_data_json['author']
 
-        # await self.channel_layer.group_send(
-        #     self.room_name,
-        #    {
-        #     "content": content,
-        #     "author": username,   
-        #     }
-        # )
-        chat = {
+        new_chat_json = {
             "content": self.content,
             "author": self.username,   
-            }
-
-        event = {"type": "send_message", "message": text_data_json}
-        await self.channel_layer.group_send(
-                self.room_name,
-                chat
-                )
+        }
+        #save to dastatbase
         await self.save_chat_to_db()
 
-        #await self.send(text_data=json.dumps(chat))
-    
+        #stream chats to room members
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send(
+            self.room_name,
+            {
+                "type": "chat_message",
+                "message": new_chat_json
+            }
+        )
+        
+    #meesage event
+    async def chat_message(self, event):
+        message = event["message"]
+
+        #Send message to websocket
+        await self.send(text_data=json.dumps(message))
 
     # save chat to database
     @database_sync_to_async
